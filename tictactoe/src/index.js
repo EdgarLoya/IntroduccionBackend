65aebd4ed2c4158071bf2bfdae10ec6d57895222
EdgarLoya <57698330+EdgarLoya@users.noreply.github.com>
2022-04-29 15:49:59 -0600

    codigo para cambio de jugadores

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -17,13 +17,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i){
@@ -35,7 +39,7 @@ class Board extends React.Component {
     );
   }
   render(){
-    const status = 'Siguiente Jugador X'
+    const status = 'Siguiente Jugador: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return(
       <div>
