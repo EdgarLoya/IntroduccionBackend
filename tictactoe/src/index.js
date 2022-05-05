be4d8fafa2b53e6100e2d9eca9e39b83d5132679
EdgarLoya <57698330+EdgarLoya@users.noreply.github.com>
2022-05-05 10:22:42 -0600

    mostrando los movimientos anteriores

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -75,6 +75,16 @@ class Game extends React.Component{
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -91,7 +101,7 @@ class Game extends React.Component{
         </div>
         <div className="game-Info">
           <div>{ status }</div>
-          <ol>{/* Todo */}</ol>
+          <ol>{ moves }</ol>
         </div>
       </div>
     );
